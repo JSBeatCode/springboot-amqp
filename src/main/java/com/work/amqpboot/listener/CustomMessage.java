6a6d34c2fdc1c8dfc8307e4c4770e7f226b96529
JS <csdnum0@gmail.com>
2023-05-28 17:24:19 +0900

    d

--- src/main/java/com/work/amqpboot/listener/CustomMessage.java
+++ src/main/java/com/work/amqpboot/listener/CustomMessage.java
@@ -19,47 +19,4 @@ public class CustomMessage {
 
 	private boolean secret;
 
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public int getPriority() {
-		return priority;
-	}
-
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
-
-	public boolean isSecret() {
-		return secret;
-	}
-
-	public void setSecret(boolean secret) {
-		this.secret = secret;
-	}
-
-	public CustomMessage(String text, int priority, boolean secret) {
-		super();
-		this.text = text;
-		this.priority = priority;
-		this.secret = secret;
-	}
-
-	public CustomMessage() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public String toString() {
-		return "CustomMessage [text=" + text + ", priority=" + priority + ", secret=" + secret + "]";
-	}
-	
-	
-
 }
